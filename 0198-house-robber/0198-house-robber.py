aa004718b6029eaeb7e5ff843a1fd53300e2b209
pranav sirnapalli <68234635+pranav-sirnapalli@users.noreply.github.com>
2025-10-29 18:45:02 -0400

    Time: 0 ms (100%), Space: 17.6 MB (82.38%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n1 = n2 = 0
+
+        for val in nums:
+            tmp = max(n1+val, n2)
+            n1 = n2
+            n2 = tmp
+        return n2
