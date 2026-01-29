207cd08762bab7a5534a081ed496751586135a9a
pranav sirnapalli <68234635+pranav-sirnapalli@users.noreply.github.com>
2026-01-29 17:29:53 -0500

    Time: 11 ms (25.58%), Space: 24.9 MB (9.27%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        buckets = [[] for _ in range(len(nums)+1)]
+
+        for num, count in freq.items():
+            buckets[count].append(num)
+        
+        res = []
+        for count in range(len(buckets)-1, 0, -1):
+            for num in buckets[count]:
+                res.append(num)
+                if len(res) == k:
+                    return res
