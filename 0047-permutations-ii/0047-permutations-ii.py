f3fb7c879b189df80cd2514f7c9a36bfc9372879
pranav sirnapalli <68234635+pranav-sirnapalli@users.noreply.github.com>
2026-02-02 18:57:07 -0500

    Time: 2 ms (94.07%), Space: 19.9 MB (34.73%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -5,16 +5,16 @@ class Solution:
         def backtrack(start):
             if start == len(nums):
                 res.append(nums[:])
-                return
             
             seen = set()
             for i in range(start, len(nums)):
                 if nums[i] in seen:
                     continue
                 seen.add(nums[i])
+
                 nums[i], nums[start] = nums[start], nums[i]
                 backtrack(start+1)
                 nums[i], nums[start] = nums[start], nums[i]
-        
+
         backtrack(0)
         return res
