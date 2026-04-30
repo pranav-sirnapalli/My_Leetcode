463421852b4521704c17372224c61090c71c93b5
pranav sirnapalli <68234635+pranav-sirnapalli@users.noreply.github.com>
2026-04-30 00:38:52 -0400

    Time: 23 ms (93.24%), Space: 19.5 MB (87.73%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -7,8 +7,10 @@ class Solution:
             for c in range(cols):
                 if grid[r][c] == 1:
                     perimeter += 4
+
                     if r > 0 and grid[r-1][c] == 1:
                         perimeter -= 2
+                    
                     if c > 0 and grid[r][c-1] == 1:
                         perimeter -= 2
         return perimeter
